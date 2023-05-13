efac01407552f66f26768b5535985ed05dc399b8
Mbrain <ennocentcip@gmail.com>
2023-05-13 15:17:46 +0100

    finish project

--- js/app.js
+++ js/app.js
@@ -7,10 +7,12 @@ const date = (document.getElementById("date").innerHTML =
 // select button and links
 const navBtn = document.getElementById("nav-toggle");
 const links = document.getElementById("nav-links");
+
 // add event listener
 navBtn.addEventListener("click", () => {
   links.classList.toggle("show-links");
 });
+
 
 // ********** smooth scroll ************
 // select links
